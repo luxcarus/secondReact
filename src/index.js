33172d1d93e619d25d3a7a1e55740e38b10e9af0
luxlee <luxlee@luxleeui-MacBook-Air.local>
2017-07-15 13:25:49 +0900

    React INDEX.js 설정 : provider 포함

--- src/index.js
+++ src/index.js
@@ -4,5 +4,22 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider  } from 'react-redux';
+// import listApp from './store/reducers';
+
+// import 'materialize-css'
+
+const store = createStore();
+
+
+const render = () => {
+    ReactDOM.render(
+        <Provider store = { store }>
+        </Provider>,
+        document.getElementById('root')
+    );
+};
+store.subscribe(render);
+render();
 registerServiceWorker();
